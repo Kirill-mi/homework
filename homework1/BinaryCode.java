88854c030dfa6cab7e17a65a973f0bd56bd26f42
Kirill Mikhalochkin <kirill-mi84@mail.ru>
2021-01-08 12:20:43 +0300

    added tasks of the second part on the topic operators

--- homework1/BinaryCode.java
+++ homework1/BinaryCode.java
@@ -2,7 +2,7 @@ package homework1;
 
 import java.util.Scanner;
 
-public class Task7 {
+public class BinaryCode {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Введи число [-128;127]: ");
