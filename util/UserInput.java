9399693fe39b04ac8f817b8d982fa6f0ac5d7a79
Kirill Mikhalochkin <kirill-mi84@mail.ru>
2021-02-05 18:41:09 +0300

    Create Util

--- util/UserInput.java
+++ util/UserInput.java
@@ -1,11 +1,20 @@
-package homework1;
+package util;
 
 import java.util.Scanner;
+
+/**
+ * @author Kirill Mikhalochkin
+ * Class of input informatiom from console
+ */
 
 public class UserInput {
     private final static Scanner SCANNER = new Scanner(System.in);
     private final static Scanner SCANNER_STRING = new Scanner(System.in);
 
+    /**
+     * @param messageToDisplay information for user
+     * @return int var, which user input from console
+     */
     public static int getUserInputInt(String messageToDisplay) {
         System.out.println(messageToDisplay);
         try {
@@ -16,11 +25,19 @@ public class UserInput {
         }
     }
 
+    /**
+     * @param messageToDisplay information for user
+     * @return String var, which user input from console
+     */
     public static String getUserInputString(String messageToDisplay) {
         System.out.println(messageToDisplay);
         return SCANNER_STRING.nextLine();
     }
 
+    /**
+     * @param messageToDisplay information for user
+     * @return int double, which user input from console
+     */
     public static double getUserInputDouble(String messageToDisplay) {
         System.out.println(messageToDisplay);
         try {
